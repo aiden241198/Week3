a24f62145f5f57dd2a6e4009d883f8b705b3c0c8
Aiden <Aiden@AIDEN-PC>
2019-02-19 01:56:17 +0000

    save

--- src/week3/ArrayOfNumbersApplication.java
+++ src/week3/ArrayOfNumbersApplication.java
@@ -1,10 +1,11 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package week3;
-
+import java.util.Arrays;
 /**
  *
  * @author Aiden
@@ -15,7 +16,21 @@ public class ArrayOfNumbersApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        int[] array = { 23, 32, 56, 44, 58, 61, 76, 80, 97, 31 };  
+        
+        //test array initialisation
+        ArrayOfNumbers a = new ArrayOfNumbers(10);
+        a.item(7);
+        
+        //test array index change
+        ArrayOfNumbers b = new ArrayOfNumbers(10);
+        b.setItem(7, 3);
+       
+        //test Max item in array
+        ArrayOfNumbers c = new ArrayOfNumbers(10);
+        c.max();
     }
     
 }
